bc310f63a8d7bc16067345a555efbbe6b597bb7b
EngiBeerHub <engibeer0422@gmail.com>
2023-08-13 15:27:59 +0900

    [feature/12]add: top page
    - deleted default content of AppComponent
    - added directory structure
    - added top page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TopComponent } from './components/pages/top/top.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/top', pathMatch: 'prefix' },
+  { path: 'top', component: TopComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TopComponent } from './components/pages/top/top.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [AppComponent, TopComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })

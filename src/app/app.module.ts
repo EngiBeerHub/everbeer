9af1489e6e3d6266792b31cd0800d698ebd5f9a3
EngiBeerHub <engibeer0422@gmail.com>
2023-08-13 20:19:35 +0900

    [feature/19]add: implement material card
    - implemented card
    - styling a bit
    - set alternative image when null

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TopComponent } from './components/pages/top/top.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [AppComponent, TopComponent],
@@ -15,6 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/pages/top/top.component.ts
+++ src/app/components/pages/top/top.component.ts
@@ -8,12 +8,18 @@ import { BeerService } from 'src/app/services/beer.service';
 })
 export class TopComponent implements OnInit {
   beer: Beer | undefined;
+  imageUrl: string | undefined;
+  readonly altImageUrl = 'https://images.punkapi.com/v2/keg.png';
 
   constructor(private beerService: BeerService) {}
 
   ngOnInit(): void {
     this.beerService.getRandomBeer().subscribe((fetchedBeer) => {
       this.beer = fetchedBeer;
+      // set alternative keg image when image url is null.
+      this.beer.image_url
+        ? (this.imageUrl = this.beer.image_url)
+        : (this.imageUrl = this.altImageUrl);
     });
   }
 

0c7be54c1b80a619d6cf10984953c711d2d2d674
EngiBeerHub <engibeer0422@gmail.com>
2023-08-13 21:18:31 +0900

    [feature/21]add: spinner

--- src/app/components/pages/top/top.component.ts
+++ src/app/components/pages/top/top.component.ts
@@ -8,18 +8,25 @@ import { BeerService } from 'src/app/services/beer.service';
 })
 export class TopComponent implements OnInit {
   beer: Beer | undefined;
+  isLoading: boolean | undefined;
   imageUrl: string | undefined;
   readonly altImageUrl = 'https://images.punkapi.com/v2/keg.png';
 
   constructor(private beerService: BeerService) {}
 
   ngOnInit(): void {
+    // start loading to show spinner
+    this.isLoading = true;
+
     this.beerService.getRandomBeer().subscribe((fetchedBeer) => {
       this.beer = fetchedBeer;
       // set alternative keg image when image url is null.
       this.beer.image_url
         ? (this.imageUrl = this.beer.image_url)
         : (this.imageUrl = this.altImageUrl);
+
+      // stop loading to hide spinner
+      this.isLoading = false;
     });
   }
 

93ffe23ea4f5cc438584786b0ec6a7e2f4bdb17d
EngiBeerHub <engibeer0422@gmail.com>
2023-08-15 23:30:10 +0900

    [feature/22]add: split components
    - refactored random beer to a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,10 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { BeerCardComponent } from './components/presentationals/beer-card/beer-card.component';
+import { RandomBeerComponent } from './components/containers/random-beer/random-beer.component';
 
 @NgModule({
-  declarations: [AppComponent, TopComponent, BeerCardComponent],
+  declarations: [AppComponent, TopComponent, BeerCardComponent, RandomBeerComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/components/containers/random-beer/random-beer.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Beer } from 'src/app/models/beer';
+import { BeerService } from 'src/app/services/beer.service';
+
+@Component({
+  selector: 'app-random-beer',
+  templateUrl: './random-beer.component.html',
+  styleUrls: ['./random-beer.component.scss'],
+})
+export class RandomBeerComponent implements OnInit {
+  beer?: Beer;
+  isLoading?: boolean;
+  imageUrl?: string;
+  readonly altImageUrl = 'https://images.punkapi.com/v2/keg.png';
+
+  constructor(private beerService: BeerService) {}
+
+  ngOnInit(): void {
+    // start loading to show spinner
+    this.isLoading = true;
+
+    this.beerService.getRandomBeer().subscribe((fetchedBeer) => {
+      this.beer = fetchedBeer;
+      // set alternative keg image when image url is null.
+      this.beer.image_url
+        ? (this.imageUrl = this.beer.image_url)
+        : (this.imageUrl = this.altImageUrl);
+
+      // stop loading to hide spinner
+      this.isLoading = false;
+    });
+  }
+}

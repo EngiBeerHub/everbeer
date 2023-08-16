bdb44aab91090f460316de3c6937963dbb3f313f
EngiBeerHub <engibeer0422@gmail.com>
2023-08-16 22:56:45 +0900

    [feature/25]add: fixed failed tests to succeed

--- src/app/components/pages/top/top-page.component.spec.ts
+++ src/app/components/pages/top/top-page.component.spec.ts
@@ -1,14 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TopPageComponent } from './top-page.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { RandomBeerComponent } from '../../containers/random-beer/random-beer.component';
+import { BeerService } from 'src/app/services/beer.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-describe('TopComponent', () => {
+describe('TopPageComponent', () => {
   let component: TopPageComponent;
   let fixture: ComponentFixture<TopPageComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [TopPageComponent],
+      declarations: [TopPageComponent, RandomBeerComponent],
+      imports: [
+        MatToolbarModule,
+        MatIconModule,
+        MatProgressSpinnerModule,
+        HttpClientTestingModule,
+      ],
+      providers: [BeerService],
     });
     fixture = TestBed.createComponent(TopPageComponent);
     component = fixture.componentInstance;

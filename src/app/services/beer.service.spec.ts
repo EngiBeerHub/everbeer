300c6eec3c6741283dc274e9ee6aeab134de522c
EngiBeerHub <engibeer0422@gmail.com>
2023-08-17 22:37:25 +0900

    feature/29 add: simple test for http request

--- src/app/services/beer.service.spec.ts
+++ src/app/services/beer.service.spec.ts
@@ -1,9 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { BeerService } from './beer.service';
+import { Beer } from '../models/beer';
+import { mockRandomBeer } from '../testing/mock-beer';
 
 describe('BeerService', () => {
+  let httpTestingController: HttpTestingController;
   let service: BeerService;
 
   beforeEach(() => {
@@ -11,9 +17,32 @@ describe('BeerService', () => {
       imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(BeerService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    // Verify no request remained
+    httpTestingController.verify();
+  });
+
+  // it('should be created', () => {
+  //   expect(service).toBeTruthy();
+  // });
+
+  it('should get random beer from server', () => {
+    const expected: Beer[] = mockRandomBeer;
+
+    // Assert response
+    service.getRandomBeer().subscribe((actual) => {
+      expect(actual).toEqual(expected[0]);
+    });
+
+    const req = httpTestingController.expectOne(
+      'https://api.punkapi.com/v2/beers/random',
+    );
+
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(expected);
   });
 });

010eea93e034c8d9d4d663cff8127a73f5a104ef
EngiBeerHub <engibeer0422@gmail.com>
2023-09-10 18:55:43 +0900

    [feature/8]move responsive grid

--- src/app/components/containers/beer-list/beer-list.component.ts
+++ src/app/components/containers/beer-list/beer-list.component.ts
@@ -1,3 +1,4 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 import { Beer } from 'src/app/models/beer';
 import { BeerService } from 'src/app/services/beer.service';
@@ -11,17 +12,44 @@ export class BeerListComponent implements OnInit {
   beers?: Beer[];
   // vars for grid list
   gridCols = 3;
-  gridGutterSize = '24px';
-  gridRowHeight = 550;
+  gridGutterSize = '16px'; // plus 8px margin of each cards
+  gridRowHeight = 570;
 
-  constructor(private beerService: BeerService) {}
+  constructor(
+    private beerService: BeerService,
+    private breakpointObserver: BreakpointObserver,
+  ) {}
 
   ngOnInit(): void {
+    // Fetch all beers from API
     this.beerService.getAllBeers().subscribe({
       next: (fetchedBeers) => {
         this.beers = fetchedBeers;
       },
+      // TODO: error:
       complete: () => console.log('getAllBeers completed.'),
     });
+
+    // Observe some predefined breakpoints and set the grid accordingly.
+    this.breakpointObserver
+      .observe([
+        Breakpoints.HandsetLandscape,
+        Breakpoints.TabletLandscape,
+        Breakpoints.HandsetPortrait,
+        Breakpoints.HandsetLandscape,
+      ])
+      .subscribe((result) => {
+        const breakpoints = result.breakpoints;
+        // TODO: configure more detail
+        if (breakpoints[Breakpoints.TabletPortrait]) {
+          this.gridCols = 1;
+        } else if (breakpoints[Breakpoints.HandsetPortrait]) {
+          this.gridCols = 1;
+        } else if (breakpoints[Breakpoints.HandsetLandscape]) {
+          this.gridCols = 1;
+        } else if (breakpoints[Breakpoints.TabletLandscape]) {
+          this.gridCols = 2;
+        }
+      });
   }
 }

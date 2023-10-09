70e01e45458a068c1706fc542c4abe9f410fea52
EngiBeerHub <engibeer0422@gmail.com>
2023-10-09 15:53:19 +0900

    [feature/9] fixed routing setting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TopPageComponent } from './components/pages/top/top-page.component';
+import { BeerDetailComponent } from './components/pages/beer-detail/beer-detail.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/top', pathMatch: 'prefix' },
+  { path: '', redirectTo: '/top', pathMatch: 'full' },
   { path: 'top', component: TopPageComponent },
+  { path: 'detail', component: BeerDetailComponent },
 ];
 
 @NgModule({
